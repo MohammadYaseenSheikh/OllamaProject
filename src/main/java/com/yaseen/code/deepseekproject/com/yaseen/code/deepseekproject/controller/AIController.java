e31266993dcf79797291781912304da96b57df39
Yaseen Sheikh <official.yaseen.sheikh@gmail.com>
2025-02-06 01:44:12 +0530

    UI Implementation.

--- src/main/java/com/yaseen/code/deepseekproject/com/yaseen/code/deepseekproject/controller/AIController.java
+++ src/main/java/com/yaseen/code/deepseekproject/com/yaseen/code/deepseekproject/controller/AIController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/Ollama")
+@RequestMapping("/ai")
 public class AIController {
 
     @Autowired
