05043156387a476513b365cafb6b1ca817fe734d
Yaseen Sheikh <official.yaseen.sheikh@gmail.com>
2025-02-13 15:14:25 +0530

    * Reverted - Google Login Implementation

--- src/main/java/com/yaseen/code/deepseekproject/com/yaseen/code/deepseekproject/config/AppConfig.java
+++ src/main/java/com/yaseen/code/deepseekproject/com/yaseen/code/deepseekproject/config/AppConfig.java
@@ -2,9 +2,6 @@ package com.yaseen.code.deepseekproject.com.yaseen.code.deepseekproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -13,27 +10,6 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login").permitAll() // Allow login page access
-                        .anyRequest().authenticated() // Protect other endpoints
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/oauth2/authorization/google") // Use the default Spring OAuth2 login page
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .oidcUserService(this.oidcUserService())
-                        )
-                ); // Enable Google OAuth2 Login
-        return http.build();
-    }
-
-    private OidcUserService oidcUserService() {
-        OidcUserService delegate = new OidcUserService();
-        return delegate;
     }
 
 }
