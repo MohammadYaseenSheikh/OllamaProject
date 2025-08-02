c5b28af76935160b3eed065a108a7f5b8fb06681
Mohammad Yaseen Sheikh Akram <33812949+MohammadYaseenSheikh@users.noreply.github.com>
2025-08-02 19:07:25 +0530

    Add test security configuration and enable test profile in pipeline configuration

---
+++ src/test/java/DeepseekProjectApplicationTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DeepseekProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
